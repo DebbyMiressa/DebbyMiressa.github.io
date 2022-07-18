140228bf8617540ba0288207e86515babc5da231
Debby Miressa <101793279+DebbyMiressa@users.noreply.github.com>
2022-07-18 18:47:13 +0300

    Update style.css and index.html
    Fix stylelint and eslint errors; install eslint  and configure it in package-lock.json and package.json

--- js/script.js
+++ js/script.js
@@ -1,19 +1,19 @@
-let menu  = document.querySelector('#menu')
-let humberIcon = document.querySelector('.humber-icon')
-let humber = document.querySelector('.humber')
-let menuItem = document.querySelectorAll('.navigate')
+const menu = document.querySelector('#menu');
+const humberIcon = document.querySelector('.humber-icon');
+const humber = document.querySelector('.humber');
+const menuItem = document.querySelectorAll('.navigate');
 
-function activeHumber () {
-    humber.classList.add('active-humber');
+function activeHumber() {
+  humber.classList.add('active-humber');
 }
 
-function disableHumber () {
-    humber.classList.remove('active-humber');
+function disableHumber() {
+  humber.classList.remove('active-humber');
 }
 
-menu.addEventListener('click',activeHumber)
-humberIcon.addEventListener('click',disableHumber)
+menu.addEventListener('click', activeHumber);
+humberIcon.addEventListener('click', disableHumber);
 
-menuItem.forEach(item => {
-    item.addEventListener('click',disableHumber) 
-})
+menuItem.forEach((item) => {
+  item.addEventListener('click', disableHumber);
+});
