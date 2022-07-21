af9a845936e8c0693d9d2557438eba3067c81276
benjamin kisenge <benkisenge03@gmail.com>
2022-07-21 21:38:31 +0200

    scripted form validation

--- js/validation.js
+++ js/validation.js
@@ -1,21 +1,29 @@
-/* eslint-disable linebreak-style */
+/* eslint-disable linebreak-style */ 
 const email = document.querySelector('#email');
 const submit = document.querySelector('#submit');
 const label = document.querySelector('.validate');
+const btn = document.querySelector('.form-inputs button '); 
 
-const isLowerCase = (str) => {
-  const regExp = /[A-Z]/;
-  const isMatch = regExp.test(str);
-  if (str === '') return false;
-  const result = !isMatch;
-  return result;
+const isLowerCase = (str) => { 
+    const regExp = /[A-Z]/; 
+    const isMatch = regExp.test(str); 
+    if (str === '') return false; 
+    const result = !isMatch; 
+    return result; 
 };
 
-submit.addEventListener('click', (e) => {
-  if (!isLowerCase(email.value)) {
-    label.innerHTML = 'E-mail is invalid!!';
-    e.preventDefault();
-  } else {
-    label.innerHTML = '';
-  }
-});
+submit.addEventListener('click', (e) => { 
+    if (!isLowerCase(email.value)) { 
+        label.style.position = 'relative'; 
+        label.style.color = 'red'; 
+        label.style.top = '10px'; 
+        label.style.left = '10px';
+         btn.style.marginLeft = '10%';
+        label.innerHTML = 'E-mail is invalid!!'; 
+          e.preventDefault(); 
+    } 
+    else {
+       label.innerHTML = ''; 
+        btn.style.marginLeft = '32.5%'; 
+        } 
+  }); 
